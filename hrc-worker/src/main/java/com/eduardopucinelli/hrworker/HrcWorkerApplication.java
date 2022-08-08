5a493c404003f8c4f2dbd2cf02ecc17f81244412
Eduardo Pucinelli <dudupucinelli@gmail.com>
2022-08-08 02:50:47 -0300

    Created project

---
+++ hrc-worker/src/main/java/com/eduardopucinelli/hrworker/HrcWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.eduardopucinelli.hrworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrcWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrcWorkerApplication.class, args);
+	}
+
+}
---
+++ hrc-worker/src/test/java/com/eduardopucinelli/hrworker/HrcWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardopucinelli.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrcWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
