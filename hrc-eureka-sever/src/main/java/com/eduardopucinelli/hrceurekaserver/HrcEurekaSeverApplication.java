ae8cb06d3273a355c330ff96d8998c1b8288aeba
Eduardo Pucinelli <dudupucinelli@gmail.com>
2022-08-09 15:18:11 -0300

    Eureka Server created

---
+++ hrc-eureka-sever/src/main/java/com/eduardopucinelli/hrceurekaserver/HrcEurekaSeverApplication.java
@@ -0,0 +1,13 @@
+package com.eduardopucinelli.hrceurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrcEurekaSeverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrcEurekaSeverApplication.class, args);
+	}
+
+}
---
+++ hrc-eureka-sever/src/test/java/com/eduardopucinelli/hrceurekaserver/HrcEurekaSeverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardopucinelli.hrceurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrcEurekaSeverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
