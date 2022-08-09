ce6892f7da49281be8c8d2cfcd79da324643652f
Eduardo Pucinelli <dudupucinelli@gmail.com>
2022-08-09 15:40:43 -0300

    Configuring eureka clients

--- hrc-payroll/src/main/java/com/eduardopucinelli/hrpayroll/HrcPayrollApplication.java
+++ hrc-payroll/src/main/java/com/eduardopucinelli/hrpayroll/HrcPayrollApplication.java
@@ -2,11 +2,12 @@ package com.eduardopucinelli.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
-@RibbonClient(name = "hrc-worker")
+//eureka ja balanceia a carga, não precisa de ribbon
 @EnableFeignClients
+@EnableEurekaClient
 @SpringBootApplication
 public class HrcPayrollApplication {
 
