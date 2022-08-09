f73a7d60a8bb1b880e5d55a4674ee7669fdb1f2f
Eduardo Pucinelli <dudupucinelli@gmail.com>
2022-08-09 00:35:52 -0300

    Project hrpayroll created

---
+++ hrc-payroll/src/main/java/com/eduardopucinelli/hrpayroll/HrcPayrollApplication.java
@@ -0,0 +1,13 @@
+package com.eduardopucinelli.hrpayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrcPayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrcPayrollApplication.class, args);
+	}
+
+}
---
+++ hrc-payroll/src/test/java/com/eduardopucinelli/hrpayroll/HrcPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardopucinelli.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrcPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
