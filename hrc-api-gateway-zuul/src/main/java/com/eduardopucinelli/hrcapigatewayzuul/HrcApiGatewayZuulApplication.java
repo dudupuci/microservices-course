1433f4eacb2149e0df8395e156421d9e7dad095d
Eduardo Pucinelli <dudupucinelli@gmail.com>
2022-08-09 16:50:46 -0300

    Project hrc-api-gateway-zuul created

---
+++ hrc-api-gateway-zuul/src/main/java/com/eduardopucinelli/hrcapigatewayzuul/HrcApiGatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package com.eduardopucinelli.hrcapigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrcApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrcApiGatewayZuulApplication.class, args);
+	}
+
+}
---
+++ hrc-api-gateway-zuul/src/test/java/com/eduardopucinelli/hrcapigatewayzuul/HrcApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardopucinelli.hrcapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrcApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
