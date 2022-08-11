4390e370626376c804888dfb79e1ed77c5c07ef1
Eduardo Pucinelli <dudupucinelli@gmail.com>
2022-08-11 17:13:10 -0300

    Configuring hrc-server-config project

---
+++ hrc-config-server/src/test/java/com/eduardopucinelli/hrcconfigserver/HrcConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardopucinelli.hrcconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrcConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
